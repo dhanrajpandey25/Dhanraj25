85998de2213b67127d682cfef5d4a1955a6983c5
Dhanraj <pandeydhanraj25@gmail.com>
2024-02-17 13:37:19 +0530

    hello

---
+++ Untitled-1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using  namespace std;
+int main()
+{
+    int a,b,sum=0;
+    cout<<"Enter the value of a";
+    cin>>a;
+    cout<<"Enter the value of b";
+    cin>>b;
+    sum=a+b;
+    cout<<"The sum of two numbers is "<<sum;
+
+    return 0;
+}
